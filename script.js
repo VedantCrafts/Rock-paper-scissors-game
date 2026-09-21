e02729046e201e83afaabe291d55ea177217edd5
Vedant Mistry <vedantmistry578@gmail.com>
2026-09-21 18:20:59 +0000

    complete getComputerChoice function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let computer = Math.floor(Math.random()*3) + 1;
+    
+    if(computer ===1){
+        return "rock";
+    }
+    else if(computer=== 2){
+        return "paper";
+    }
+    else{
+        return "sciccors";
+    }
+}

f9d553e8cf0a7fa27dd7c036d9055af0bec87cf2
Vedant Mistry <vedantmistry578@gmail.com>
2026-09-22 15:55:13 +0000

    Complete getHumanChoice function

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function getComputerChoice(){
         return "sciccors";
     }
 }
+
+function getHumanChoice(){
+    let human = +prompt("Enter 1 for rock\n2for paper\n3for scissors");
+    
+    if(human ===1){
+        return "rock";
+    }
+    else if(human=== 2){
+        return "paper";
+    }
+    else{
+        return "scicssors";    
+    }   
+}
+let humanchoice = getHumanChoice(); 
+console.log(humanchoice);
